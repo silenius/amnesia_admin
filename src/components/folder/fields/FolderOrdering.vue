c39ffae72ccb0806dc30345cf30433f6b38dd8d9
Julien Cigar <julien@perdition.city>
2023-03-03 16:46:43 +0100

    sync

---
+++ src/components/folder/fields/FolderOrdering.vue
@@ -0,0 +1,27 @@
+<template>
+  <table class="border-collapse table-fixed">
+    <thead>
+      <tr class="text-left">
+        <th class="p-2">Field</th>
+        <th class="p-2">Direction</th>
+        <th class="p-2">Nulls</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="order in orders">
+        <td>
+          <input type="checkbox" />
+          {{ order.doc }}
+        </td>
+
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+
+<script setup>
+
+
+
+</script>
