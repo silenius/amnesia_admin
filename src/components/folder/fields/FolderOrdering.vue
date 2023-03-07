dd8812a07ee0587456e9bc73a4d3a5e3b47b03f4
Julien Cigar <julien@perdition.city>
2023-03-07 15:19:58 +0100

    allow drag and drop reordering

--- src/components/folder/fields/FolderOrdering.vue
+++ src/components/folder/fields/FolderOrdering.vue
@@ -10,7 +10,16 @@
     <tbody>
       <tr v-for="order in orders.orders" 
         :key="order.key"
-        class="cursor-move odd:bg-white even:bg-slate-50 text-slate-600"
+        :draggable="order.checked"
+        :class="{ 'cursor-move': order.checked }"
+        @drag="drag"
+        @dragstart="start"
+        @dragend="end"
+        @dragleave="leave"
+        @dragenter="enter"
+        @dragover="over"
+        @drop="drop"
+        class="odd:bg-white even:bg-slate-50 text-slate-600"
       >
         <td>
           <input type="checkbox" v-model="order.checked" />
@@ -102,5 +111,68 @@ watch(
   }, 
   { immediate: true }
 )
+
+const get_tr = (node) => {
+  let target = node
+
+  if (target.nodeType != Node.ELEMENT_NODE) {
+    target = target.parentNode
+  }
+
+  return target.closest('tr')
+}
+
+const drag = (evt) => {
+  console.debug('===> DRAG', evt)
+}
+
+const start = (evt) => {
+  console.debug('===> START', evt)
+  evt.target.classList.add('opacity-25', 'border-indigo-500', 'border')
+  evt.dataTransfer.dropEffect = "move";
+  evt.dataTransfer.setData('text/plain', evt.target.rowIndex)
+}
+
+const end = (evt) => {
+  console.debug('===> END', evt)
+  const tr = get_tr(evt.target)
+  tr.classList.remove('opacity-25', 'border-indigo-500', 'border')
+}
+
+const enter = (evt) => {
+  console.debug('===> ENTER', evt)
+  evt.preventDefault()
+}
+
+const leave = (evt) => {
+  console.debug('===> LEAVE', evt)
+
+  const tr = get_tr(evt.target)
+  tr.classList.remove('border-lime-500', 'border')
+}
+
+const over = (evt) => {
+  console.debug('===> OVER', evt)
+  evt.preventDefault()
+
+  const tr = get_tr(evt.target)
+  tr.classList.add('border-lime-500', 'border')
+}
+
+const drop = (evt) => {
+  console.debug('===> DROP', evt)
+
+  const tr = get_tr(evt.target)
+  tr.classList.remove('border-lime-500', 'border')
+
+  const from = parseInt(evt.dataTransfer.getData('text/plain')) - 1
+  const to = parseInt(tr.rowIndex) - 1
+
+  orders.value.orders.splice(
+    to, 0, orders.value.orders.splice(from, 1)[0]
+  )
+
+  evt.preventDefault()
+}
 
 </script>
