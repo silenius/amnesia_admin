6be5d545f23812ef8bfd7c5218c04cbcf9994c1c
Julien Cigar <julien@perdition.city>
2023-06-13 13:32:26 +0200

    use auth store

--- src/components/headers/DefaultHeader.vue
+++ src/components/headers/DefaultHeader.vue
@@ -9,11 +9,10 @@
     </div>
 
     <!-- LOGGED -->
-
-    <div v-if="account" class="flex justify-end sm:mr-4 ">
+    <div v-if="auth.is_logged" class="flex justify-end sm:mr-4 ">
       <Menu as="div" class="relative">
         <MenuButton>
-          <img :src="account.gravatar" class="hover:scale-125 hover:cursor-pointer transition
+          <img :src="auth.user.gravatar" class="hover:scale-125 hover:cursor-pointer transition
             duration-500 h-10 w-10 sm:h-16 sm:w-16 rounded-full" />
         </MenuButton>
 
@@ -71,11 +70,11 @@
                     class="float-right" />
                 </DialogTitle>
 
-                <form class="space-y-4 md:space-y-6" action="#">
+                <div class="space-y-4 md:space-y-6">
                   <div>
                     <label for="username" class="block mb-2 text-sm font-medium
                       text-gray-900 dark:text-white">Username</label>
-                    <input type="username" name="username" id="username"
+                    <input type="username" v-model="username" name="username" id="username"
                       class="bg-gray-50 border border-gray-300 text-gray-900
                       sm:text-sm rounded-lg focus:ring-blue-600
                       focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700
@@ -85,7 +84,7 @@
                   </div>
                   <div>
                     <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                    <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                    <input v-model="password" type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                   </div>
                   <div class="flex items-center justify-between">
                     <div class="flex items-start">
@@ -98,11 +97,11 @@
                     </div>
                     <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Forgot password?</a>
                   </div>
-                  <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
+                  <button @click.prevent="doLogin" type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
                   <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                     Don’t have an account yet? <a href="#" class="font-medium text-blue-600 hover:underline dark:text-blue-500">Sign up</a>
                   </p>
-                </form>
+                </div>
 
                   <button
                     type="button"
@@ -127,7 +126,7 @@
 
 <script setup>
 
-import { ref, onMounted } from 'vue'
+import { isRef, ref, onMounted, watch } from 'vue'
 import { RouterLink } from 'vue-router'
 import { 
   Menu, MenuButton, MenuItems, MenuItem,
@@ -135,22 +134,18 @@ import {
   TransitionRoot,
   TransitionChild,
 } from '@headlessui/vue'
-import { useAccounts } from '@/composables/accounts.js'
+import { useAuthStore } from '@/stores/auth.js'
 
-const { my, logout } = useAccounts()
+const auth = useAuthStore()
 
-const account = ref()
+const username = ref(null)
+const password = ref(null)
+
 const login_modal_open = ref(false)
+
 const closeModal = () => login_modal_open.value = false
-
-onMounted( async () => {
-  const { data } = await my()
-  account.value = data
-})
-
-const doLogout = async () => {
-  const { data } = await logout()
-  account.value = null
-}
+const openModal = () => login_modal_open.value = true
+const doLogin = async() => await auth.login(username, password)
+const doLogout = async() => await auth.logout()
 
 </script>
