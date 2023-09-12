58478f3506140d01859b9515074b5dd9d8b42c07
Julien Cigar <julien@perdition.city>
2023-09-12 09:01:33 +0200

    sync

--- src/components/headers/DefaultHeader.vue
+++ src/components/headers/DefaultHeader.vue
@@ -51,8 +51,8 @@
           >
             <TransitionChild
               as="template"
-              enter="duration-300 ease-out"
-              enter-from="opacity-0 scale-95"
+              enter="duration-500 ease-out"
+              enter-from="opacity-0 scale-75"
               enter-to="opacity-100 scale-100"
               leave="duration-200 ease-in"
               leave-from="opacity-100 scale-100"
