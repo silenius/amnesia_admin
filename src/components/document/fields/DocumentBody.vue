d1a86238db086330ab25a5abc1391a3f939bad83
Julien Cigar <julien@perdition.city>
2024-01-19 10:26:14 +0100

    replace tinymce with tiptap

--- src/components/document/fields/DocumentBody.vue
+++ src/components/document/fields/DocumentBody.vue
@@ -3,7 +3,6 @@
     <label>
       <p class="font-bold">Body</p>
       <p class="text-xs">The content of the document</p>
-      <!-- <EditorTinyMCE v-model="value" /> -->
       <EditorTipTap v-model:content="value" />
       <span class="text-red-500" v-if="errors.body">
         {{ errors.body }}
@@ -14,7 +13,6 @@
 
 <script setup>
 import { inject, computed } from 'vue'
-// import EditorTinyMCE from '@/components/content/fields/EditorTinyMCE.vue'
 import EditorTipTap from '@/components/content/fields/EditorTipTap.vue'
 import { useContent } from '@/composables/contents.js'
 
--- src/components/event/fields/EventBody.vue
+++ src/components/event/fields/EventBody.vue
@@ -3,7 +3,7 @@
     <label>
       <p class="font-bold">Body</p>
       <p class="text-xs">The content of the event</p>
-      <EditorTinyMCE v-model="value" />
+      <EditorTipTap v-model:content="value" />
       <span class="text-red-500" v-if="errors.body">
         {{ errors.body }}
       </span>
@@ -13,7 +13,7 @@
 
 <script setup>
 import { inject, computed } from 'vue'
-import EditorTinyMCE from '@/components/content/fields/EditorTinyMCE.vue'
+import EditorTipTap from '@/components/content/fields/EditorTipTap.vue'
 import { useContent } from '@/composables/contents.js'
 
 const props = defineProps({
