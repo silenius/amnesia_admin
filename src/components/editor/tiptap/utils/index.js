9cc392d4398ddb95e587f003edf36c145c704e3a
Julien Cigar <julien@perdition.city>
2024-03-26 12:04:22 +0100

    add some utils

---
+++ src/components/editor/tiptap/utils/index.js
@@ -0,0 +1,19 @@
+export const reponsive_breakpoints = new Set([
+    'sm', 'md', 'lg', 'xl', '2xl'
+])
+
+export const generate_responsive_cls = (cls) => new Set([
+    `${cls}`,
+    `sm:${cls}`,
+    `md:${cls}`,
+    `lg:${cls}`,
+    `xl:${cls}`,
+    `2xl:${cls}`,
+])
+
+export const get_modifiers = (cls) => {
+    const modifiers = cls.split(':')
+    modifiers.pop()
+
+    return modifiers
+}
