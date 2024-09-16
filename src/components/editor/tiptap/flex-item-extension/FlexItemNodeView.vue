6d723acdfdc5175f7488afc67bc2694d1f31adb4
Julien Cigar <julien@perdition.city>
2024-09-16 10:30:24 +0200

    add flex node view

---
+++ src/components/editor/tiptap/flex-item-extension/FlexItemNodeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <node-view-wrapper class="relative">
+    <span @click.prevent="deleteNode()" class="absolute right-0 translate-x-4 text-red-500">
+      <font-awesome-icon icon="fa-solid fa-xmark" class="w-4 h-4" />
+    </span>
+    <NodeViewContent />
+  </node-view-wrapper>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import { NodeViewWrapper, NodeViewContent, nodeViewProps } from '@tiptap/vue-3';
+
+const props = defineProps(nodeViewProps)
+</script>
