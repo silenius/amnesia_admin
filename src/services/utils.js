308bb24f08786dca6ae357cca4d3387dcd1329d3
Julien Cigar <julien@perdition.city>
2025-03-25 11:47:17 +0100

    add range()

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,9 +1,14 @@
 export const debounce = (callback, wait) => {
-  let timeoutId = null;
-  return (...args) => {
-    window.clearTimeout(timeoutId);
-    timeoutId = window.setTimeout(() => {
-      callback.apply(null, args);
-    }, wait);
-  };
+    let timeoutId = null;
+    return (...args) => {
+        window.clearTimeout(timeoutId);
+        timeoutId = window.setTimeout(() => {
+            callback.apply(null, args);
+        }, wait);
+    };
 }
+
+export const range = (start, stop, step) => Array.from(
+    { length: Math.ceil((stop - start) / step) },
+    (_, i) => start + i * step
+)
